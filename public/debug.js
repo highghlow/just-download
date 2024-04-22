12d06565c351db858c6c62f70e6fd4075d918750
highghlow <highghlow@proton.me>
2024-04-22 23:13:36 +0300

    Formatted code

--- public/debug.js
+++ public/debug.js
@@ -5,49 +5,51 @@ let raw_search_xml = document.getElementById("raw-search-xml");
 
 // Prettify json
 function syntaxHighlight(json) {
-    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-        var cls = 'number';
-        if (/^"/.test(match)) {
-            if (/:$/.test(match)) {
-                cls = 'key';
-            } else {
-                cls = 'string';
-            }
-        } else if (/true|false/.test(match)) {
-            cls = 'boolean';
-        } else if (/null/.test(match)) {
-            cls = 'null';
-        }
-        return '<span class="' + cls + '">' + match + '</span>';
-    });
+  json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+  return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+    var cls = 'number';
+    if (/^"/.test(match)) {
+      if (/:$/.test(match)) {
+	cls = 'key';
+      } else {
+	cls = 'string';
+      }
+    } else if (/true|false/.test(match)) {
+      cls = 'boolean';
+    } else if (/null/.test(match)) {
+      cls = 'null';
+    }
+    return '<span class="' + cls + '">' + match + '</span>';
+  });
 }
 function prettify(text) {
-	let prettified = JSON.stringify(JSON.parse(text), null, 2);
-	return syntaxHighlight(prettified);
+  let prettified = JSON.stringify(JSON.parse(text), null, 2);
+  return syntaxHighlight(prettified);
 }
 
 config_elem.innerHTML = prettify(config_elem.textContent);
 
 function search() {
-	let query = search_input.value;
+  let query = search_input.value;
 
-	fetch("/search?"+new URLSearchParams({q: query}))
-		.then((response) => response.text().then((text) => {
-			if (!response.ok) {
-				raw_response.textContent = text;
-			} else {
-				let json = JSON.parse(text);
-				raw_response.innerHTML = prettify(text);
-				raw_search_xml.replaceChildren();
-				json.raw.forEach((raw_xml) => {
-					let display_elem = document.createElement("pre");
-					display_elem.textContent = json.raw;
-					raw_search_xml.appendChild(display_elem);
-				});
-			}
-		}),
-		(error) => {
+  fetch("/search?"+new URLSearchParams({q: query}))
+    .then((response) => response.text().then((text) => {
+      if (!response.ok) {
+	raw_response.textContent = text;
+      } else {
 
-		})
+	let json = JSON.parse(text);
+	raw_response.innerHTML = prettify(text);
+	raw_search_xml.replaceChildren();
+	
+	json.raw.forEach((raw_xml) => {
+	  let display_elem = document.createElement("pre");
+	  display_elem.textContent = json.raw;
+	  raw_search_xml.appendChild(display_elem);
+	});
+      }
+    }),
+    (error) => {
+
+    })
 }
--- public/index.js
+++ public/index.js
@@ -6,124 +6,123 @@ let indexer_errors_elem = document.getElementById("indexer-errors");
 setInterval(update_download_progress, 5000);
 
 input_field.addEventListener("keypress", function(event) {
-	if (event.key === "Enter") {
-		event.preventDefault();
-		document.getElementById("search-button").click();
-	}
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("search-button").click();
+  }
 });
 
 function download(event) {
-	console.log(event.target);
-	event.target.textContent = "Requesting...";
+  console.log(event.target);
+  event.target.textContent = "Requesting...";
 	
-	fetch("/download", {method: "POST", body: new URLSearchParams({url: event.target.dataset.download_url})})
-		.then((response) => {
-			if (response.ok) {
-				event.target.textContent = "Downloading...";
-			} else {
-				event.target.textContent = "Failed!";
-			}
-		});
+  fetch("/download", {method: "POST", body: new URLSearchParams({url: event.target.dataset.download_url})})
+    .then((response) => {
+      if (response.ok) {
+	event.target.textContent = "Downloading...";
+      } else {
+	event.target.textContent = "Failed!";
+      }
+    });
 
-	update_download_progress();
+    update_download_progress();
 }
 
 function displayDownloadInfo(elem, torrent) {
-	let titleElem = document.createElement("span");
-	titleElem.classList.add("torrent-title")
-	titleElem.textContent = torrent.title;
+  let titleElem = document.createElement("span");
+  titleElem.classList.add("torrent-title")
+  titleElem.textContent = torrent.title;
 
-	let downloadPercentage = document.createElement("span");
-	downloadPercentage.classList.add("download-percentage");
-	downloadPercentage.textContent = torrent.percent;
+  let downloadPercentage = document.createElement("span");
+  downloadPercentage.classList.add("download-percentage");
+  downloadPercentage.textContent = torrent.percent;
 
-	let etaElem = document.createElement("span");
-	etaElem.classList.add("eta");
-	etaElem.textContent = torrent.eta_text;
-
-	elem.replaceChildren(titleElem, downloadPercentage, etaElem);
+  let etaElem = document.createElement("span");
+  etaElem.classList.add("eta");
+  etaElem.textContent = torrent.eta_text;
+  elem.replaceChildren(titleElem, downloadPercentage, etaElem);
 }
 
 function update_download_progress() {
-	fetch("/progress")
-		.then((response) => response.json()
-			.then((data) => data.forEach((torrent) => {
-				let download_progress_container = document.getElementById("download-progress");
-				let display_elem = Array.from(download_progress_container.children).find((elem) => elem.dataset.id == torrent.id);
+  fetch("/progress")
+    .then((response) => response.json()
+      .then((data) => data.forEach((torrent) => {
+	let download_progress_container = document.getElementById("download-progress");
+	let display_elem = Array.from(download_progress_container.children).find((elem) => elem.dataset.id == torrent.id);
 
-				if (display_elem == null) {
-					display_elem = document.createElement("div");
-					display_elem.dataset.id = torrent.id;
-					display_elem.classList = ["download-box"];
-					download_progress_container.appendChild(display_elem);
-				}
-				display_elem.dataset.status = torrent.status;
+	if (display_elem == null) {
+	  display_elem = document.createElement("div");
+	  display_elem.dataset.id = torrent.id;
+	  display_elem.classList = ["download-box"];
+	  download_progress_container.appendChild(display_elem);
+	}
+	display_elem.dataset.status = torrent.status;
 
-				displayDownloadInfo(display_elem, torrent);
-			})),
-			// On fail
-			(error) => search_notice.textContent = "Failed! ("+error+")")
+      	displayDownloadInfo(display_elem, torrent);
+      })),
+      // On fail
+      (error) => search_notice.textContent = "Failed! ("+error+")")
 }
 update_download_progress();
 
 function displayErrors(elem, errors) {
-	errors.forEach((error) => {
-		console.log(error);
+  errors.forEach((error) => {
+    console.log(error);
 		
-		let error_elem = document.createElement("p");
-		error_elem.textContent = "Indexer " + error.indexer + " failed with code " + error.code;
+    let error_elem = document.createElement("p");
+    error_elem.textContent = "Indexer " + error.indexer + " failed with code " + error.code;
 
-		elem.appendChild(error_elem);
-	})
+    elem.appendChild(error_elem);
+  })
 }
 
 function search() {
-	let query = document.getElementById("search-field").value;
+  let query = document.getElementById("search-field").value;
 
-	let first_child = search_results_elem.replaceChildren(search_results_elem.firstChild);
+  let first_child = search_results_elem.replaceChildren(search_results_elem.firstChild);
 
-	search_notice.textContent = "Searching...";
+  search_notice.textContent = "Searching...";
 
-	fetch("/search?" + new URLSearchParams({q: query}), {method: "GET"})
-		.then((response) => response.json())
-		.then((data) => {
-			search_notice.textContent = "";
-			if (data.results.length == 0) { search_notice.textContent = "No results" }
+  fetch("/search?" + new URLSearchParams({q: query}), {method: "GET"})
+    .then((response) => response.json())
+    .then((data) => {
+      search_notice.textContent = "";
+      if (data.results.length == 0) { search_notice.textContent = "No results" }
 
-			indexer_errors_elem.replaceChildren();
-			displayErrors(indexer_errors_elem, data.errors);
+      indexer_errors_elem.replaceChildren();
+      displayErrors(indexer_errors_elem, data.errors);
 
-			data.results.forEach((search_result) => {
-				let table_row = document.createElement("tr");
+      data.results.forEach((search_result) => {
+	let table_row = document.createElement("tr");
 
-				let title_elem = document.createElement("td");
-				title_elem.textContent = search_result.description;
+	let title_elem = document.createElement("td");
+	title_elem.textContent = search_result.description;
 
-				let size_elem = document.createElement("td");
-				size_elem.textContent = humanFileSize(search_result.size);
-				size_elem.dataset.sort_value = search_result.size;
+	let size_elem = document.createElement("td");
+	size_elem.textContent = humanFileSize(search_result.size);
+	size_elem.dataset.sort_value = search_result.size;
 
-				let seeders_elem = document.createElement("td");
-				seeders_elem.textContent = search_result.seeders;
-				seeders_elem.dataset.sort_value = search_result.seeders;
+	let seeders_elem = document.createElement("td");
+	seeders_elem.textContent = search_result.seeders;
+	seeders_elem.dataset.sort_value = search_result.seeders;
 
-				let peers_elem = document.createElement("td");
-				peers_elem.textContent = search_result.peers;
-				peers_elem.dataset.sort_value = search_result.peers;
+	let peers_elem = document.createElement("td");
+	peers_elem.textContent = search_result.peers;
+	peers_elem.dataset.sort_value = search_result.peers;
 
-				let download_elem = document.createElement("td");
-				let download_button = document.createElement("button");
+	let download_elem = document.createElement("td");
+	let download_button = document.createElement("button");
 
-				download_button.textContent = "Download";
-				download_button.dataset.download_url = search_result.link;
-				download_button.addEventListener("click", download);
+	download_button.textContent = "Download";
+	download_button.dataset.download_url = search_result.link;
+	download_button.addEventListener("click", download);
 
-				download_elem.replaceChildren(download_button);
+	download_elem.replaceChildren(download_button);
 
-				table_row.replaceChildren(title_elem, size_elem, seeders_elem, peers_elem, download_elem);
-				search_results_elem.appendChild(table_row);
-			})
-		});
+	table_row.replaceChildren(title_elem, size_elem, seeders_elem, peers_elem, download_elem);
+	search_results_elem.appendChild(table_row);
+      })
+  });
 }
 
 function humanFileSize(bytes, si=false, dp=1) {
